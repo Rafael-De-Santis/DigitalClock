f34aaacff335f9a541bbcca485b21c4ab955d97f
Firehun <javmoravszky@gmail.com>
2023-02-05 06:38:12 +0100

    Latest update

--- public/app.js
+++ public/app.js
@@ -2,6 +2,8 @@ const hourEle = document.getElementById("hour")
 const minuteEle = document.getElementById("minutes")
 const secondEle = document.getElementById("seconds")
 const ampmEle = document.getElementById("ampm")
+const dataExtract = document.querySelector("#dataExtract");
+
 
 var timeZone = 0;
   
@@ -72,43 +74,43 @@ function backgroundChange(clockArray){
         case intHour >= 6 && dayNight === "AM" && intHour < 12:
             document.body.style.backgroundImage = "url(images/1.jpg)";
             rgb = "black"
-            console.log("6 a 12 am - sunrise");
+            //console.log("6 a 12 am - sunrise");
             break;
             //**Noon
         case intHour == 12 && dayNight === "PM" || dayNight === "PM" && intHour < 2:
             document.body.style.backgroundImage = "url(images/2.jpg)";
             rgb = "white"
-            console.log("12 a 2pm - noon");
+            //console.log("12 a 2pm - noon");
             break;
             //*Afternoon
         case intHour >= 2 && dayNight === "PM" && intHour < 5:
             document.body.style.backgroundImage = "url(images/3.jpg)";
             rgb = "white"
-            console.log("2 a 5 pm - afternoon");
+            //console.log("2 a 5 pm - afternoon");
             break;
             //*DAWN
         case intHour >= 5 && dayNight === "PM" && intHour < 7:
             document.body.style.backgroundImage = "url(images/4.jpg)";
             rgb = "white"
-            console.log("5 a 7 pm - dawn");
+            //console.log("5 a 7 pm - dawn");
             break;
             //*7pm
         case intHour >= 7 && dayNight === "PM" && intHour < 9:
             document.body.style.backgroundImage = "url(images/5.jpg)";
             rgb = "white"
-            console.log("7 a 9pm - 7pm");
+            //console.log("7 a 9pm - 7pm");
             break;
             //*NIGHT
-        case intHour >= 9 && dayNight === "PM" && intHour < 12 || (intHour < 3 && dayNight === "AM"):
+        case intHour >= 9 && dayNight === "PM" && intHour < 12 || (intHour == 12 && dayNight === "AM") || (intHour < 3 && dayNight === "AM"):
             document.body.style.backgroundImage = "url(images/6.jpg)";
             rgb = "white"
-            console.log("9pm a 3am - night");
+            //console.log("9pm a 3am - night");
             break;
             //*DARKNIGHT
         case intHour >= 3 && dayNight === "AM" && intHour < 6:
             document.body.style.backgroundImage = "url(images/7.jpg)";
             rgb = "white"
-            console.log("3 a 6 am - darknight");
+            //console.log("3 a 6 am - darknight");
     }
     
 colorChange(rgb);
@@ -122,21 +124,26 @@ var buttons = document.querySelector('.buttons')
 var dropdown = document.querySelector("#countries")
 
 //https://countryflagsapi.com/:filetype/:code
-const apiFlags = "https://countryflagsapi.com/png/"
+const apiFlags = "https://countryflagsapi.com/png/";
+const apiCountries = "https://api.bigdatacloud.net/data/country-info?code=";
+const apiCountries2 = "&localityLanguage=en&key=";
+//https://api.bigdatacloud.net/data/country-info?code=au&localityLanguage=en&key=[YOUR API KEY]
+const apiCountriesKey = "bdc_e3419f5841bb43b6bb3802f41d16dbb5";
+//apiCountries+Countries2[x][4]+apiCountries2+apiCountriesKey
 
 const Countries2 = {
-    utc: ["England", "0", "Tea", "gb-eng"],   
-    venezuela: ["Venezuela","-4","Arepa", "ve"],
-    hungria: ["Hungria","1","Gyulas","hu"],
-    china: ["China", "8", "Aloz", "cn"],
-    chile: ["Chile", "-3", "Sopaipilla", "cl"],
-    usaeast:["USEast", "-5", "TortaCubana", "us"],
-    usawest:["USWest", "-8", "Tacos", "us"],
-    guatemala:["Guatemala", "-6", "Rafael", "gt"],
-    spain:["Spain", "1", "Paella", "es"],
-    canadaeast:["CanadaEast", "-4", "Poutine", "ca"],
-    canadawest:["CanadaWest", "-8", "JapaDog", "ca"],
-    mexico:["Mexico", "-6", "Chilaquiles", "mx"]
+    utc: ["England", "0", "Tea", "gb-eng", "gb"],   
+    venezuela: ["Venezuela","-4","Arepa", "ve", "ve"],
+    hungria: ["Hungria","1","Gyulas","hu", "hu"],
+    china: ["China", "8", "Aloz", "cn", "cn"],
+    chile: ["Chile", "-3", "Sopaipilla", "cl", "cl"],
+    usaeast:["USEast", "-5", "TortaCubana", "us", "us"],
+    usawest:["USWest", "-8", "Tacos", "us", "us"],
+    guatemala:["Guatemala", "-6", "Rafael", "gt", "gt"],
+    spain:["Spain", "1", "Paella", "es", "es"],
+    canadaeast:["CanadaEast", "-4", "Poutine", "ca", "ca"],
+    canadawest:["CanadaWest", "-8", "JapaDog", "ca", "ca"],
+    mexico:["Mexico", "-6", "Chilaquiles", "mx", "mx"]
 }
 
 
@@ -153,13 +160,58 @@ dropdown.addEventListener('change', (e) => {
     let choice = e.target.value;
     let selected = Countries2[choice];
     var flags = document.getElementById("flags"); 
-    let countryFlag = selected[3];    
+    let countryFlag = selected[3];
+    let countryCode = selected[4];
+    let countryApiUrl = apiCountries+countryCode+apiCountries2+apiCountriesKey;
+ 
     flags.src = apiFlags+selected[3];
     timeZone = selected[1];
     clock();
     clockArray = clock();
     backgroundChange(clockArray)
+
+    dataExtract.children.innerText += "";
+
+    async function getapi(url){
+        const response = await fetch(url);
+
+        var data = await response.json();
+
+        dataExtract.children[0].innerHTML = "Codigo Alpha: "+data.isoAlpha3;
+        dataExtract.children[1].innerHTML = "Native Language: "+data.isoAdminLanguages[0].nativeName;
+        dataExtract.children[2].innerHTML = "Region: "+data.unRegion;
+        dataExtract.children[3].innerHTML = "Currency Code: "+data.currency.code;
+        dataExtract.children[4].innerHTML = "Currency Name: "+data.currency.name;
+        dataExtract.children[5].innerHTML = "Income Level: "+data.wbIncomeLevel.value;
+        dataExtract.children[6].innerHTML = "Calling Code: +"+data.callingCode;
+        dataExtract.children[7].innerHTML = "Emoji: "+data.countryFlagEmoji;
+        dataExtract.children[8].innerHTML = "Geoname ID: "+data.geonameId;
+
+
+        // let dataArray = [];
+        // dataArray.push(data.isoAlpha3, 
+        //     data.isoAdminLanguages[0].nativeName, 
+        //     data.unRegion, 
+        //     data.currency.code, 
+        //     data.currency.name, 
+        //     data.wbIncomeLevel.value, 
+        //     data.callingCode, 
+        //     data.countryFlagEmoji, 
+        //     data.geonameId);
+        //console.log(dataArray);
+        // for(var i = 0; i <= dataArray.length ; i++){
+            
+        //     console.log(dataArray);
+        //     dataExtract.children[i].innerText =" "+dataArray[i];
+            
+        // }
+
+        return dataArray;
+    }
+    getapi(countryApiUrl);    
+    
 })
+
 
 function load(){
 var bttons = ""
@@ -176,6 +228,7 @@ dropdown.innerHTML= options
 
 load()
 
+//calculates time outside clock for it to change the time on the click event function
 function calcTimeZone(timeZone){
     
     date = new Date(); //createds date object
